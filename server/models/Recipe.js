4cc4f37db39a7be29fcc639b9e7a5f98aebc0f92
Marcus <eleuz.mt@gmail.com>
2024-08-28 04:19:50 +0800

    Adjusted schema
    Submission page and recipe page have been adjusted to display the adjusted schema

--- server/models/Recipe.js
+++ server/models/Recipe.js
@@ -23,10 +23,57 @@ const recipeSchema = new mongoose.Schema({
         enum: ['Thai', 'American', 'Chinese', 'Mexican', 'Indian'],
         required: 'This field is required!'
     },
+    diets: {
+        type: [String],
+        enum: ['Vegetarian', 'Vegan', 'Gluten-Free', 'Dairy-Free'],
+        required: 'This field is required!'
+    },
+    duration: {
+        type: Number,
+        required: 'This field is required'
+    },
+    persons: {
+        type: Number,
+        required: 'This field is required'
+    },
     image : {
         type: String,
         required: 'This field is required!'
+    },
+    // Nutritional Information
+    calories: {
+        type: Number,
+        set: value => Math.round(value * 10) / 10 // Ensures a single decimal place
+    },
+    fats: {
+        type: Number,
+        set: value => Math.round(value * 10) / 10
+    },
+    carbohydrates: {
+        type: Number,
+        set: value => Math.round(value * 10) / 10 
+    },
+    protein: {
+        type: Number,
+        set: value => Math.round(value * 10) / 10
+    },
+    sugar: {
+        type: Number,
+        set: value => Math.round(value * 10) / 10
+    },
+    salt: {
+        type: Number,
+        set: value => Math.round(value * 10) / 10
+    },
+    saturates: {
+        type: Number,
+        set: value => Math.round(value * 10) / 10
+    },
+    fibre: {
+        type: Number,
+        set: value => Math.round(value * 10) / 10
     }
+    
 });
 
 recipeSchema.index({ name: 'text', description: 'text' }); //Passing recipe schema here
